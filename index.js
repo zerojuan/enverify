91f36fe4ac3cc8f8079181766f8ef8df58096433
Julius Cebreros <me@juliuscebreros.com>
2016-09-21 23:44:30 +1200

    Can load from directory

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-console.log( 'I am here' );
+const loader = require( './lib/file-loader' );
+
+// read from options file
+const path = './example';
+const opts = {};
+
+// read from folder
+loader( path, opts, ( err, configFiles ) => {
+    console.log( configFiles );
+    // keep in memory
+
+    // validate duplicates
+
+    // validate consistent shape
+
+    // validate ensure no typos
+} );
