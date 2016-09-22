ea44bf8d8715ce1e5044582f543693f3782301c4
Julius <julius@cloud.house>
2016-09-22 14:18:59 +1200

    Parameterize path to check

--- index.js
+++ index.js
@@ -4,8 +4,26 @@ const loader = require( './lib/loader' );
 const verify = require( './lib/verify' );
 
 // read from options file
-const path = './example';
+const path = process.argv.slice( 2 )[ 0 ];
+
+if ( !path ) {
+    console.error( 'Error: Specify path to inspect' );
+    process.exit( 1 );
+}
 const opts = {};
 
 // read from folder
-loader( path, opts, verify );
+loader( path, opts, ( err, configData ) => {
+    if ( err ) {
+        console.error( err );
+        return process.exit( 1 );
+    }
+
+    verify( configData, opts );
+} );
+
+process.on( 'exit', ( code ) => {
+    if ( code ) {
+        console.error( 'Failed' );
+    }
+} );
